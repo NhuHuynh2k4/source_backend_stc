8853d40f5b8981eff0c68feef801be5b23fe6c98
NGUYEN_KHUYEN\biika <khyenvippro722@gmail.com>
2024-10-19 00:24:22 +0700

    Mark và Exam

--- source_backend/sourc_backend_stc/Data/AppDbContext.cs
+++ source_backend/sourc_backend_stc/Data/AppDbContext.cs
@@ -9,5 +9,8 @@ namespace sourc_backend_stc.Data
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Class> Classes { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Mark> Marks { get; set; }
+        public DbSet<Exam> Exams { get; set; }
     }
 }
