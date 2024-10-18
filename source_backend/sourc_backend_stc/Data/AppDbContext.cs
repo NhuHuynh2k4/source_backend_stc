471abe740dd00525244d7fa69d200e8b0ea466f0
nhutphi <phivnpc06272@gmail.com>
2024-10-19 02:11:53 +0700

    api question và subject
    api question và subject

--- source_backend/sourc_backend_stc/Data/AppDbContext.cs
+++ source_backend/sourc_backend_stc/Data/AppDbContext.cs
@@ -7,7 +7,12 @@ namespace sourc_backend_stc.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        // Định nghĩa các DbSet cho các thực thể trong ứng dụng
         public DbSet<Student> Students { get; set; }
         public DbSet<Class> Classes { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Question> Questions { get; set; }
+
+        // Tùy chỉnh cấu hình Entity Framework thông qua phương thức OnModelCreating
     }
 }
